9e15f05b96976029948dccba90a447975871f654
Bhaumik <bhaumik.ptl86@gmail.com>
2024-07-29 00:38:35 -0400

    commiting all project files

--- components/translator.js
+++ components/translator.js
@@ -4,7 +4,124 @@ const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
 class Translator {
+    constructor(){
+        this.matched = []
+        this.result = []
+        this.resultText = ''
+    }
 
+    checkAmericanBritish(locale){
+        let searchDict;
+        if(locale === 'american-to-british'){
+            searchDict = americanOnly
+        }
+        else{
+            searchDict = britishOnly
+        }
+        let text = this.resultText
+        const arrAmericanBritish = Object.keys(searchDict);
+        const regex = new RegExp(`\\b(${arrAmericanBritish.join('|')})\\b`, "gi")
+        const parts = text.match(regex)?.filter(Boolean).map(word => word.trim());
+        
+        parts?.forEach(el => {
+            if(text.match(el)){
+                this.matched.push(searchDict[el.toLowerCase()])
+                text = text.replace(el, searchDict[el.toLowerCase()])
+            }
+        })
+        this.resultText = text
+    }
+
+    checkSpelling(locale){
+        let searchDict;
+        if(locale === 'american-to-british'){
+            searchDict = americanToBritishSpelling
+        }
+        else{
+            searchDict = Object.fromEntries(Object.entries(americanToBritishSpelling).map(([key,val]) => [val, key]));
+        }
+        let text = this.resultText
+        const regex = /[^\w\s']/g
+        for (let prop in searchDict){
+            if(text.replace(regex, '').split(' ').includes(prop)){
+                this.matched.push(searchDict[prop])
+                text = text.replace(prop, searchDict[prop])
+            }
+        }
+        this.resultText = text
+    }
+
+    checkTitle(locale){
+        let searchDict;
+        if(locale === 'american-to-british'){
+            searchDict = americanToBritishTitles
+        }
+        else{
+            searchDict = Object.fromEntries(Object.entries(americanToBritishTitles).map(([key,val]) => [val, key]));
+        }
+        let text = this.resultText
+        const arrTitles = Object.keys(searchDict)
+        const regex = new RegExp(`(${arrTitles.map(keyword => keyword.replace('.', '\\.')).join('|')})`, "gi")
+        const parts = text.match(regex)?.filter(Boolean).map(word => word.trim());
+        
+        parts?.forEach(el => {
+            if(text.match(el)){
+                if(el[0] === el[0].toUpperCase()){
+                    const replacement = searchDict[el.toLowerCase()]
+                    this.matched.push(replacement[0].toUpperCase() + replacement.slice(1))
+                    text = text.replace(el, replacement[0].toUpperCase() + replacement.slice(1))
+                }
+                if(el[0] === el[0].toLowerCase()){
+                    this.matched.push(searchDict[el.toLowerCase()])
+                    text = text.replace(el, searchDict[el.toLowerCase()])
+                }
+                
+            }
+        });
+        this.resultText = text
+    }
+
+    checkTime(locale){
+        let regex;
+        if(locale === 'american-to-british'){
+            regex = /(\d+)\:(\d{2})/g
+        }else{
+            regex = /(\d+)\.(\d{2})/g
+        }
+        let text = this.resultText
+        const parts = text.match(regex)?.filter(Boolean).map(word => word.trim());
+
+        parts?.forEach(el => {
+            if(text.match(el)){
+                if(regex.toString().includes(':')){
+                    this.matched.push(el.replace(':', '.'))
+                    text = text.replace(el, el.replace(':', '.'))
+                }
+                if(regex.toString().includes('.')){
+                    this.matched.push(el.replace('.', ':'))
+                    text = text.replace(el, el.replace('.', ':'))
+                }
+            }
+        });
+        this.resultText = text
+    }
+
+    translateText(text, locale){
+        this.resultText = text
+        this.checkSpelling(locale);
+        this.checkAmericanBritish(locale);
+        this.checkTime(locale)
+        this.checkTitle(locale)
+        console.log(this.resultText, this.matched);
+    }
+
+    highlightTranslation(){
+        let highlighted = this.resultText
+        this.matched.forEach(el => {
+            highlighted = this.resultText.replace(el, `<span class="highlight">${el}</span>`)
+        });
+        this.resultText = highlighted
+    }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,24 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      const {text, locale} = req.body
+      if(text === ''){
+        res.json({ error: 'No text to translate' })
+      }
+      else if(!text || !locale){
+        res.json({ error: 'Required field(s) missing' })
+      }
+      else if(locale !== 'american-to-british'  && locale !== 'british-to-american'){
+        res.json({ error: 'Invalid value for locale field' })
+      }
+      else{
+        translator.translateText(text, locale)
+        if(text === translator.resultText){
+          res.json({text: text, translation: 'Everything looks good to me!'})
+        }else{
+          translator.highlightTranslation()
+          res.json({text: text, translation: translator.resultText})
+        }
+      }
     });
 };
